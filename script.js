a163c966cf8bff59453d0af086209a9f51e7e9ec
graziilindaa <162333286+graziilindaa@users.noreply.github.com>
2024-06-25 09:30:44 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,19 +6,52 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Pergunta 1",
+        enunciado: "são 6 da manhã e o despertador tocou e você levantou, você vai:",
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+            "tomar banho",
+            "escovar os dentes"
         ]
     },
     {
-        enunciado: "Pergunta 2",
+        enunciado: "ótima escolha, agora você deseja:",
         alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
+            "se arrumar para escola",
+            "tomar café da manhã"
         ]
-    }
+    },
+    {
+        enunciado: "você está pronto para ir à escola, você vai:",
+        alternativas: [
+            "levar algo para comer na escola",
+            "comprar algo na cantina da escola"
+        ]
+    },
+    {
+        enunciado: "você chegou à escola, você vai:",
+        alternativas: [
+            "ir para sala",
+            "ir encher a garafa"
+        ]
+    },
 ]; 
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+function mostraAlternativas() {
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativa.textContent = alternativa;
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+mostraPergunta();
+
+
 
 
